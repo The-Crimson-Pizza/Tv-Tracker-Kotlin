9099ed10622e921e0a76ecf6bea21e3909918535
Andrea Lobo <andrea.lobo93@gmail.com>
2020-06-25 19:05:12 +0200

    Initial commit

---
+++ app/src/main/java/com/thecrimsonpizza/tvtrackerkotlin/tvtrackerkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thecrimsonpizza.tvtrackerkotlin.tvtrackerkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
